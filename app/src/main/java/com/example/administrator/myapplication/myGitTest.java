1c18ff264bdc795d8f28c2118278f9913f5293f1
yaolunhui <2358667573@qq.com>
2016-09-20 11:10:56 +0800

    添加add函数

--- app/src/main/java/com/example/administrator/myapplication/myGitTest.java
+++ app/src/main/java/com/example/administrator/myapplication/myGitTest.java
@@ -4,4 +4,8 @@ package com.example.administrator.myapplication;
  * Created by Administrator on 2016/9/19.
  */
 public class myGitTest {
+    public void add(int i,int j)
+    {
+        int z=i+j;
+    }
 }
